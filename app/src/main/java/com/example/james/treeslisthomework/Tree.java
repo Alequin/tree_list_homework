0cd1ee33731025321d32450f21d15af3ed6c7898
James Cox <j-cox03@hotmail.com>
2017-07-05 16:46:47 +0100

    Tree: made constructor in class Tree

--- app/src/main/java/com/example/james/treeslisthomework/Tree.java
+++ app/src/main/java/com/example/james/treeslisthomework/Tree.java
@@ -5,4 +5,17 @@ package com.example.james.treeslisthomework;
  */
 
 public class Tree {
+
+    private String name;
+    private String countryOfOrigin;
+    private String seedType;
+    private int averageHeight;
+
+    public Tree(String name, String countryOfOrigin, String seedType, int averageHeight){
+        this.name = name;
+        this.countryOfOrigin = countryOfOrigin;
+        this.seedType = seedType;
+        this.averageHeight = averageHeight;
+    }
+
 }
