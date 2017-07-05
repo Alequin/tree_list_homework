4c9e97ab41ba27545967093bcb57bd30df338801
James Cox <j-cox03@hotmail.com>
2017-07-05 18:22:59 +0100

    ListActivity: Made and set list adapter

--- app/src/main/java/com/example/james/treeslisthomework/ListActivity.java
+++ app/src/main/java/com/example/james/treeslisthomework/ListActivity.java
@@ -2,6 +2,8 @@ package com.example.james.treeslisthomework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        TreeListAdapter treeAdapter = new TreeListAdapter(this, TreesDatabase.getTrees());
+        listView.setAdapter(treeAdapter);
     }
 }
