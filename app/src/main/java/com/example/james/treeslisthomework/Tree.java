9d16fc17525650dad0b4e2d2dede347ee780f63d
James Cox <j-cox03@hotmail.com>
2017-07-05 16:41:14 +0100

    inital commit

---
+++ app/src/main/java/com/example/james/treeslisthomework/Tree.java
@@ -0,0 +1,8 @@
+package com.example.james.treeslisthomework;
+
+/**
+ * Created by james on 05/07/2017.
+ */
+
+public class Tree {
+}
---
+++ app/src/main/java/com/example/james/treeslisthomework/TreesDatabase.java
@@ -0,0 +1,12 @@
+package com.example.james.treeslisthomework;
+
+/**
+ * Created by james on 05/07/2017.
+ */
+
+public class TreesDatabase {
+
+    public TreesDatabase(){
+
+    }
+}
